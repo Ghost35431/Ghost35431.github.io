4a525bfcf3e66fc27ff429af093334f6e759c0ec
Ghost35431 <98342525+Ghost35431@users.noreply.github.com>
2022-01-24 13:56:44 -0600

    Create imps.py

---
+++ code/python/test/imps.py
@@ -0,0 +1,17 @@
+import os
+import sys
+import cmath
+import pygame
+import math
+
+class consol:
+  def echo(temp):
+    _input = "echo" + temp
+    os.system(_input)
+  pi = math.pi
+
+def test(argsv):
+  echo(args)
+
+def main():
+  print("testing testing")
