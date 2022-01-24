1394ec3534d4456485f00ed0581cb77014c351b0
Ghost35431 <98342525+Ghost35431@users.noreply.github.com>
2022-01-24 14:00:55 -0600

    Update main.py

--- code/python/test/main.py
+++ code/python/test/main.py
@@ -2,11 +2,12 @@ import os
 import sys
 import cmath
 import math
-import test from imps
+import imps
 
 args = sys.argv
 
 def main():
+  consol.echo('testing')
   test(args)
 
 if __name__ == "__main__":
